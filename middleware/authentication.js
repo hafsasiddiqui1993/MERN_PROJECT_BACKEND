de9a9c7c717a6407efbd421977eb2e5ea2605fa5
Hafsa Saad <120678121+hafsasiddiqui1993@users.noreply.github.com>
2023-03-15 18:18:37 +0500

    Add files via upload

---
+++ middleware/authentication.js
@@ -0,0 +1,28 @@
+const JWT = require('jsonwebtoken');
+const SecKey = "Hello"
+function authentication (req, res, next){
+
+    const Token = req.headers.tokenization; 
+console.log(Token)
+console.log(req.headers)
+
+try{    
+if (Token==null) {
+      res.status(400).send("You are not authenticate");
+    } else {
+      JWT.verify(Token, SecKey, (err, other) => {
+        if (err) {
+          console.log(err)
+          res.status(401).send(err);
+        } 
+
+        
+        req.Another = other;
+        next();
+      });
+    }
+    } catch(err){
+      req.status(401).send(err)
+    }
+  }
+module.exports = authentication
