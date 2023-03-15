734f56f0af109df5e27a45ff84a50e716bb4cb90
Hafsa Saad <120678121+hafsasiddiqui1993@users.noreply.github.com>
2023-03-16 01:31:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 const cors = require("cors");
-app.use(cors(*))
-
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(bodyParser.json());
 
