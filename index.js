802828d445511fc0c6376e0aaed6de1b8bbe7678
Hafsa Saad <saaddhaf@corp.contoursoftware.net>
2023-03-16 15:47:40 +0500

    my 3rd commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 const cors = require("cors");
-// app.use(cors({origin:"*"}))
+app.use(cors({origin:"*"}))
 
 
 app.use(bodyParser.json());
